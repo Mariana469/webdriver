ba2fcebec1a43ea61bc3716df2d9ac13c0a9f707
Mariana469 <75381906+Mariana469@users.noreply.github.com>
2021-05-24 18:45:41 +0300

    Update HelloWebDriver.java

--- HelloWebDriver.java
+++ HelloWebDriver.java
@@ -1,14 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWebDriver {
     public static void main(String[] args) throws InterruptedException {
-        ChromeDriver driver;
-            System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver_win32\\chromedriver.exe");
-            driver = new ChromeDriver();
+  //      ChromeDriver driver;
+  //      System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver_win32\\chromedriver.exe");
+  //      driver = new ChromeDriver();
+
+  //      driver.get("http://seleniumhg.org");
+  //      Thread.sleep(2000);
+  //      driver.quit();
+
+        System.setProperty("webdriver.FireFox.driver", "C:\\webdrivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
 
         driver.get("http://seleniumhg.org");
         Thread.sleep(2000);
         driver.quit();
     }
 }
+
