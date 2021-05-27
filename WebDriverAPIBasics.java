46386f1061be9247fe34525cb00e811d68a0ad37
Mariana469 <75381906+Mariana469@users.noreply.github.com>
2021-05-27 17:39:30 +0300

    Create WebDriverAPIBasics.java

---
+++ WebDriverAPIBasics.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverAPIBasics {
+    public static void main(String[] args) throws InterruptedException {
+             ChromeDriver driver;
+        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://seleniumhg.org");
+        driver.switchTo().frame(driver.findElementByXPath("//iframe[@id='slave-1-1']"));
+        WebElement searchInput = driver.findElement(By.xpath("//input[@title='Enter a search']"));
+        searchInput.sendKeys("selenium java");
+
+        WebElement searchBtn;
+        driver.findElement(By.className("sbb")).click();
+
+        driver.findElement(By.className("sale_banner_orange")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    };
+
+}
