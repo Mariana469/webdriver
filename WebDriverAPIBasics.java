dd32767b722eb18c4c43f0d709c32f8861b6f520
Mariana469 <75381906+Mariana469@users.noreply.github.com>
2021-05-28 13:25:40 +0300

    Update WebDriverAPIBasics.java

--- WebDriverAPIBasics.java
+++ WebDriverAPIBasics.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +14,7 @@ public class WebDriverAPIBasics {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://seleniumhg.org");
+        
         driver.switchTo().frame(driver.findElementByXPath("//iframe[@id='slave-1-1']"));
         WebElement searchInput = driver.findElement(By.xpath("//input[@title='Enter a search']"));
         searchInput.sendKeys("selenium java");
@@ -19,6 +22,8 @@ public class WebDriverAPIBasics {
         WebElement searchBtn;
         driver.findElement(By.className("sbb")).click();
 
+        WebElement sale_banner_orange = new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("sale_banner_orange")));
         driver.findElement(By.className("sale_banner_orange")).click();
         Thread.sleep(2000);
         driver.quit();
